3b77af79586051fe0355a6d926c766ccc5176ed6
Oliver Stott <dev@oliver-stott.co.uk>
2022-01-20 22:17:09 +0000

    add 2 and 3 test cases

--- test/romanNumerals.spec.js
+++ test/romanNumerals.spec.js
@@ -1,12 +1,22 @@
-function numberToRomanNumeral() {
-  return 'I';
-}
+function numberToRomanNumeral(number) {
+  let numeral = "";
+  for (let i = 0; i < number; i += 1) {
+    numeral += "I";
+  }
 
+  return numeral;
+}
 
 describe("romanNumerals", () => {
   describe("numberToRomanNumeral", () => {
-    it('converts 1 to I', () => {
-      expect(numberToRomanNumeral(1)).toEqual('I');
+    it("converts 1 to I", () => {
+      expect(numberToRomanNumeral(1)).toEqual("I");
+    });
+    it("converts 2 to II", () => {
+      expect(numberToRomanNumeral(2)).toEqual("II");
+    });
+    it("converts 3 to III", () => {
+      expect(numberToRomanNumeral(3)).toEqual("III");
     });
   });
 });
