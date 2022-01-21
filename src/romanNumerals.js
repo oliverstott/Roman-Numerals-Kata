5bed1d6c925b13cd15e508d00a0d97d30b4a53fa
Oliver Stott <dev@oliver-stott.co.uk>
2022-01-21 11:52:52 +0000

    created test cases up to 10 - not happy with current structure howerver

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,13 +1,23 @@
 export function numberToRomanNumeral(number) {
   let numeral = "";
 
-  if (number >= 4) {
+  if (number >= 9) {
+    if (number === 9) {
+      numeral += "I";
+    }
+    numeral += "X";
+  } else if (number >= 4) {
     if (number === 4) {
       numeral += "I";
     }
+
     numeral += "V";
+
+    if (number >= 5) {
+      numeral += numberToRomanNumeral(number - 5);
+    }
   } else {
-    for (let i = 0; i < number; i += 1) {
+    for (let i = 0; i < number; i++) {
       numeral += "I";
     }
   }
--- test/romanNumerals.spec.js
+++ test/romanNumerals.spec.js
@@ -17,5 +17,20 @@ describe("romanNumerals", () => {
     it("converts 5 to V", () => {
       expect(numberToRomanNumeral(5)).toEqual("V");
     });
+    it("converts 6 to VI", () => {
+      expect(numberToRomanNumeral(6)).toEqual("VI");
+    });
+    it("converts 7 to VII", () => {
+      expect(numberToRomanNumeral(7)).toEqual("VII");
+    });
+    it("converts 8 to VIII", () => {
+      expect(numberToRomanNumeral(8)).toEqual("VIII");
+    });
+    it("converts 9 to IX", () => {
+      expect(numberToRomanNumeral(9)).toEqual("IX");
+    });
+    it("converts 10 to X", () => {
+      expect(numberToRomanNumeral(10)).toEqual("X");
+    });
   });
 });
