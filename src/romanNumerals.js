81b31d24257b8353aa0e9871216b8bd11ddb53e8
Oliver Stott <dev@oliver-stott.co.uk>
2022-01-21 12:10:57 +0000

    test passes, extracted letter I to a const

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,16 +1,16 @@
 export function numberToRomanNumeral(number) {
+  const NUMERAL_ONE = "I";
   let numeral = "";
 
   if (number >= 9) {
     if (number === 9) {
-      numeral += "I";
+      numeral += NUMERAL_ONE;
     }
-    
-    numeral += "X";
 
+    numeral += "X";
   } else if (number >= 4) {
     if (number === 4) {
-      numeral += "I";
+      numeral += NUMERAL_ONE;
     }
 
     numeral += "V";
