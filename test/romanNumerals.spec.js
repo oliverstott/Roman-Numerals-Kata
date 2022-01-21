4149b62e8d1b58ec0c10822c35b4f713fa4a5160
Oliver Stott <dev@oliver-stott.co.uk>
2022-01-21 18:54:12 +0000

    case tests passed cleaning up code for refactoring stage

--- test/romanNumerals.spec.js
+++ test/romanNumerals.spec.js
@@ -2,41 +2,49 @@ import { numberToRomanNumeral } from "../src/romanNumerals";
 
 describe("romanNumerals", () => {
   describe("numberToRomanNumeral", () => {
-    it("converts 1 to I", () => {
-      expect(numberToRomanNumeral(1)).toEqual("I");
+    describe("convert up to 5", () => {
+      it("converts 1 to I", () => {
+        expect(numberToRomanNumeral(1)).toEqual("I");
+      });
+      it("converts 2 to II", () => {
+        expect(numberToRomanNumeral(2)).toEqual("II");
+      });
+      it("converts 3 to III", () => {
+        expect(numberToRomanNumeral(3)).toEqual("III");
+      });
+      it("converts 4 to IV", () => {
+        expect(numberToRomanNumeral(4)).toEqual("IV");
+      });
+      it("converts 5 to V", () => {
+        expect(numberToRomanNumeral(5)).toEqual("V");
+      });
     });
-    it("converts 2 to II", () => {
-      expect(numberToRomanNumeral(2)).toEqual("II");
+
+    describe("convert up to 10", () => {
+      it("converts 6 to VI", () => {
+        expect(numberToRomanNumeral(6)).toEqual("VI");
+      });
+      it("converts 7 to VII", () => {
+        expect(numberToRomanNumeral(7)).toEqual("VII");
+      });
+      it("converts 8 to VIII", () => {
+        expect(numberToRomanNumeral(8)).toEqual("VIII");
+      });
+      it("converts 9 to IX", () => {
+        expect(numberToRomanNumeral(9)).toEqual("IX");
+      });
+      it("converts 10 to X", () => {
+        expect(numberToRomanNumeral(10)).toEqual("X");
+      });
     });
-    it("converts 3 to III", () => {
-      expect(numberToRomanNumeral(3)).toEqual("III");
-    });
-    it("converts 4 to IV", () => {
-      expect(numberToRomanNumeral(4)).toEqual("IV");
-    });
-    it("converts 5 to V", () => {
-      expect(numberToRomanNumeral(5)).toEqual("V");
-    });
-    it("converts 6 to VI", () => {
-      expect(numberToRomanNumeral(6)).toEqual("VI");
-    });
-    it("converts 7 to VII", () => {
-      expect(numberToRomanNumeral(7)).toEqual("VII");
-    });
-    it("converts 8 to VIII", () => {
-      expect(numberToRomanNumeral(8)).toEqual("VIII");
-    });
-    it("converts 9 to IX", () => {
-      expect(numberToRomanNumeral(9)).toEqual("IX");
-    });
-    it("converts 10 to X", () => {
-      expect(numberToRomanNumeral(10)).toEqual("X");
-    });
-    it("converts 11 to XI", () => {
-      expect(numberToRomanNumeral(11)).toEqual("XI");
-    });
-    it("converts 12 to XII", () => {
-      expect(numberToRomanNumeral(12)).toEqual("XII");
+    
+    describe("convert past 10", () => {
+      it("converts 11 to XI", () => {
+        expect(numberToRomanNumeral(11)).toEqual("XI");
+      });
+      it("converts 12 to XII", () => {
+        expect(numberToRomanNumeral(12)).toEqual("XII");
+      });
     });
   });
 });
